c91c0288c09c96dfeaa18b2233261c08d821dfc6
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-02 11:53:59 +0200

    Update register.php

--- views/register.php
+++ views/register.php
@@ -1,1 +1,27 @@
+<h1>Inscription</h1>
 
+<?php if (!empty($errors)): ?>
+    <ul class="errors">
+        <?php foreach ($errors as $error): ?>
+            <li><?= htmlspecialchars($error) ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+<form action="index.php?route=register" method="post">
+    <div>
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" value="<?= htmlspecialchars($data['username'] ?? '') ?>" required>
+    </div>
+    <div>
+        <label for="email">Email :</label>
+        <input type="email" id="email" name="email" value="<?= htmlspecialchars($data['email'] ?? '') ?>" required>
+    </div>
+    <div>
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required>
+    </div>
+    <button type="submit">S'inscrire</button>
+</form>
+
+<p>Déjà un compte ? <a href="index.php?route=login">Se connecter</a></p>
