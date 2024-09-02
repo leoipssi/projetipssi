cd198d7932668aacee22bbd2d6f434802dd38b32
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-02 11:48:18 +0200

    Update HomeController.php

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -1,1 +1,21 @@
+<?php
+class HomeController {
+    public function index() {
+        $recentVehicles = Vehicle::getRecentVehicles(5);
+        $activeOffers = RentalOffer::getActiveOffers(3);
+        
+        $content = $this->render('home', ['recentVehicles' => $recentVehicles, 'activeOffers' => $activeOffers]);
+        $this->renderLayout($content);
+    }
 
+    private function render($view, $data = []) {
+        extract($data);
+        ob_start();
+        include "views/{$view}.php";
+        return ob_get_clean();
+    }
+
+    private function renderLayout($content) {
+        include 'views/layouts/main.php';
+    }
+}
