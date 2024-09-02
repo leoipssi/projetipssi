90a3ed86fcafed037bcea8dd59173351c985ce49
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-02 11:58:42 +0200

    Update vehicules.php

--- views/vehicules.php
+++ views/vehicules.php
@@ -1,1 +1,11 @@
+<h1>Nos véhicules</h1>
 
+<?php foreach ($vehicles as $vehicle): ?>
+    <div class="vehicle-card">
+        <h2><?= htmlspecialchars($vehicle->getMarque() . ' ' . $vehicle->getModele()) ?></h2>
+        <p>Couleur: <?= htmlspecialchars($vehicle->getCouleur()) ?></p>
+        <p>Immatriculation: <?= htmlspecialchars($vehicle->getImmatriculation()) ?></p>
+        <p>Kilométrage: <?= htmlspecialchars($vehicle->getKilometres()) ?> km</p>
+        <a href="index.php?route=vehicles&action=show&id=<?= $vehicle->getId() ?>">Voir détails</a>
+    </div>
+<?php endforeach; ?>
