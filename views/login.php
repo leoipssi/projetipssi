d0b7a92d6fee714ebaf27e10ec769f83be4066bf
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-02 11:53:34 +0200

    Update login.php

--- views/login.php
+++ views/login.php
@@ -1,1 +1,19 @@
+<h1>Connexion</h1>
 
+<?php if (isset($error)): ?>
+    <p class="error"><?= htmlspecialchars($error) ?></p>
+<?php endif; ?>
+
+<form action="index.php?route=login" method="post">
+    <div>
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" id="username" name="username" required>
+    </div>
+    <div>
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required>
+    </div>
+    <button type="submit">Se connecter</button>
+</form>
+
+<p>Pas encore de compte ? <a href="index.php?route=register">S'inscrire</a></p>
