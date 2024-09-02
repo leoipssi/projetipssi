182a28ee7ab6f5b0cf58a7a28ce975c1f9a993db
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-02 12:00:50 +0200

    Update vehicules.php

--- views/vehicules.php
+++ views/vehicules.php
@@ -9,3 +9,20 @@
         <a href="index.php?route=vehicles&action=show&id=<?= $vehicle->getId() ?>">Voir détails</a>
     </div>
 <?php endforeach; ?>
+
+<h1><?= htmlspecialchars($vehicle->getMarque() . ' ' . $vehicle->getModele()) ?></h1>
+
+<div class="vehicle-details">
+    <p>Numéro de série: <?= htmlspecialchars($vehicle->getNumeroSerie()) ?></p>
+    <p>Couleur: <?= htmlspecialchars($vehicle->getCouleur()) ?></p>
+    <p>Immatriculation: <?= htmlspecialchars($vehicle->getImmatriculation()) ?></p>
+    <p>Kilométrage: <?= htmlspecialchars($vehicle->getKilometres()) ?> km</p>
+    <p>Date d'achat: <?= htmlspecialchars($vehicle->getDateAchat()) ?></p>
+    <p>Prix d'achat: <?= htmlspecialchars($vehicle->getPrixAchat()) ?> €</p>
+</div>
+
+<?php if (isLoggedIn()): ?>
+    <a href="index.php?route=rentals&action=create&vehicle_id=<?= $vehicle->getId() ?>">Louer ce véhicule</a>
+<?php else: ?>
+    <p>Connectez-vous pour louer ce véhicule.</p>
+<?php endif; ?>
