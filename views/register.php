6291374597bb06a1d9ffd9b6f130f116e95c8f03
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-04 09:50:44 +0200

    Update register.php

--- views/register.php
+++ views/register.php
@@ -1,5 +1,4 @@
 <h1>Inscription</h1>
-
 <?php if (!empty($errors)): ?>
     <ul class="errors">
         <?php foreach ($errors as $error): ?>
@@ -7,8 +6,16 @@
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
-
 <form action="index.php?route=register" method="post">
+    <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrfToken) ?>">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" value="<?= htmlspecialchars($_POST['nom'] ?? '') ?>" required>
+    </div>
+    <div>
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" value="<?= htmlspecialchars($_POST['prenom'] ?? '') ?>" required>
+    </div>
     <div>
         <label for="username">Nom d'utilisateur :</label>
         <input type="text" id="username" name="username" value="<?= htmlspecialchars($_POST['username'] ?? '') ?>" required>
@@ -21,7 +28,26 @@
         <label for="password">Mot de passe :</label>
         <input type="password" id="password" name="password" required>
     </div>
+    <div>
+        <label for="password_confirm">Confirmer le mot de passe :</label>
+        <input type="password" id="password_confirm" name="password_confirm" required>
+    </div>
+    <div>
+        <label for="adresse">Adresse :</label>
+        <input type="text" id="adresse" name="adresse" value="<?= htmlspecialchars($_POST['adresse'] ?? '') ?>" required>
+    </div>
+    <div>
+        <label for="code_postal">Code postal :</label>
+        <input type="text" id="code_postal" name="code_postal" value="<?= htmlspecialchars($_POST['code_postal'] ?? '') ?>" required>
+    </div>
+    <div>
+        <label for="ville">Ville :</label>
+        <input type="text" id="ville" name="ville" value="<?= htmlspecialchars($_POST['ville'] ?? '') ?>" required>
+    </div>
+    <div>
+        <label for="telephone">Téléphone :</label>
+        <input type="tel" id="telephone" name="telephone" value="<?= htmlspecialchars($_POST['telephone'] ?? '') ?>" required>
+    </div>
     <button type="submit">S'inscrire</button>
 </form>
-
 <p>Déjà un compte ? <a href="index.php?route=login">Se connecter</a></p>
