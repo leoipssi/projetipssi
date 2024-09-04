512b71eaa572fda58ed6e9c9922b00537861aa9a
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-04 16:25:43 +0200

    Update HomeController.php

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -4,19 +4,8 @@ class HomeController extends BaseController {
         $recentVehicules = Vehicule::getRecentVehicules(5);
         $activeOffers = RentalOffer::getActiveOffers(3);
         
-        $content = $this->render('home', ['recentVehicules' => $recentVehicules, 'activeOffers' => $activeOffers]);
-        $this->renderLayout($content);
+        $this->render('home', [
+            'recentVehicules' => $recentVehicules,
+            'activeOffers' => $activeOffers
+        ]);
     }
-
-    // Changez la visibilité de cette méthode en 'protected' ou 'public'
-    protected function render($view, $data = []) {
-        extract($data);
-        ob_start();
-        include "views/{$view}.php";
-        return ob_get_clean();
-    }
-
-    protected function renderLayout($content) {
-        include 'views/layouts/main.php';
-    }
-}
