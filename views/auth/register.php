0a84bd960b295cd162214ad81c63c0742774e5a1
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-04 12:32:35 +0200

    Update register.php

--- views/auth/register.php
+++ views/auth/register.php
@@ -15,20 +15,20 @@
             <div class="row mb-3">
                 <div class="col-md-6">
                     <label for="nom" class="form-label">Nom</label>
-                    <input type="text" class="form-control" id="nom" name="nom" required>
+                    <input type="text" class="form-control" id="nom" name="nom" value="<?= htmlspecialchars($userData['nom'] ?? '') ?>" required>
                 </div>
                 <div class="col-md-6">
                     <label for="prenom" class="form-label">Prénom</label>
-                    <input type="text" class="form-control" id="prenom" name="prenom" required>
+                    <input type="text" class="form-control" id="prenom" name="prenom" value="<?= htmlspecialchars($userData['prenom'] ?? '') ?>" required>
                 </div>
             </div>
             <div class="mb-3">
                 <label for="username" class="form-label">Nom d'utilisateur</label>
-                <input type="text" class="form-control" id="username" name="username" required>
+                <input type="text" class="form-control" id="username" name="username" value="<?= htmlspecialchars($userData['username'] ?? '') ?>" required>
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Adresse e-mail</label>
-                <input type="email" class="form-control" id="email" name="email" required>
+                <input type="email" class="form-control" id="email" name="email" value="<?= htmlspecialchars($userData['email'] ?? '') ?>" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Mot de passe</label>
@@ -40,21 +40,21 @@
             </div>
             <div class="mb-3">
                 <label for="adresse" class="form-label">Adresse</label>
-                <input type="text" class="form-control" id="adresse" name="adresse" required>
+                <input type="text" class="form-control" id="adresse" name="adresse" value="<?= htmlspecialchars($userData['adresse'] ?? '') ?>" required>
             </div>
             <div class="row mb-3">
                 <div class="col-md-4">
                     <label for="code_postal" class="form-label">Code postal</label>
-                    <input type="text" class="form-control" id="code_postal" name="code_postal" required>
+                    <input type="text" class="form-control" id="code_postal" name="code_postal" value="<?= htmlspecialchars($userData['code_postal'] ?? '') ?>" required>
                 </div>
                 <div class="col-md-8">
                     <label for="ville" class="form-label">Ville</label>
-                    <input type="text" class="form-control" id="ville" name="ville" required>
+                    <input type="text" class="form-control" id="ville" name="ville" value="<?= htmlspecialchars($userData['ville'] ?? '') ?>" required>
                 </div>
             </div>
             <div class="mb-3">
                 <label for="telephone" class="form-label">Téléphone</label>
-                <input type="tel" class="form-control" id="telephone" name="telephone" required>
+                <input type="tel" class="form-control" id="telephone" name="telephone" value="<?= htmlspecialchars($userData['telephone'] ?? '') ?>" required>
             </div>
             <button type="submit" class="btn btn-primary w-100">S'inscrire</button>
         </form>
