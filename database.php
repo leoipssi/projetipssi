b88dd91df58c1d0b8448d9f817f20df03239d9ac
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-10 14:27:32 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,12 +1,13 @@
-?php
+<?php
 $servername = "localhost";
 $username = "emotion_user";
 $password = "IPSSI2024";
 $database = "e_motion";
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $db = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base de données.";
 } catch(PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
