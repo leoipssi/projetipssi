8c6a5a96c07725c8f349b31cb0d1fea805f1afa4
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-10 10:20:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Configurations de session avant le démarrage de la session
-ini_set('session.gc_maxlifetime', 3600)
+ini_set('session.gc_maxlifetime', 3600);
 ini_set('session.cookie_lifetime', 3600);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_strict_mode', 1);
@@ -8,24 +8,30 @@ ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_secure', 1); // Uniquement si HTTPS est utilisé
 // Démarrage de la session
 session_start();
+
 // Fonction de logging personnalisée
 function custom_log($message) {
     error_log(date('Y-m-d H:i:s') . ': ' . $message);
 }
+
 // Logging des informations de session
 custom_log('Session ID: ' . session_id());
 custom_log('Session Data: ' . json_encode($_SESSION));
+
 require_once 'config.php';
 require_once 'helpers.php';
 require_once 'vendor/autoload.php';
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+
 if (file_exists('database.php')) {
     require_once 'database.php';
 } else {
     die("Le fichier de configuration de la base de données est manquant.");
 }
+
 spl_autoload_register(function($class) {
     $directories = ['models', 'controllers'];
     foreach ($directories as $directory) {
@@ -37,10 +43,16 @@ spl_autoload_register(function($class) {
     }
     throw new Exception("La classe {$class} n'a pas été trouvée.");
 });
+
 $logger = new \Monolog\Logger('app');
 $logger->pushHandler(new \Monolog\Handler\StreamHandler('logs/app.log', \Monolog\Logger::DEBUG));
+
 // Détermine la route actuelle
 $route = $_GET['route'] ?? 'home';
+
+// Créer une instance de AuthController
+$authController = new AuthController($logger);
+
 // Gestion des erreurs
 try {
     switch ($route) {
@@ -51,12 +63,11 @@ try {
         case 'login':
         case 'register':
         case 'logout':
-            $controller = new AuthController($logger);
-            $controller->$route();
+            $authController->$route();
             break;
         case 'admin':
             // Vérifie les autorisations d'accès
-            if (!AuthController::isAdmin()) {
+            if (!$authController->isAdmin()) {
                 $logger->warning("Accès refusé à la page d'administration pour un utilisateur non administrateur.");
                 header('Location: index.php?route=home');
                 exit;
@@ -83,7 +94,7 @@ try {
             break;
         case 'rentals':
             // Vérifie si l'utilisateur est connecté
-            if (!AuthController::checkLoggedIn()) {
+            if (!$authController->isLoggedIn()) {
                 $logger->warning("Tentative d'accès à la page de locations sans connexion.");
                 header('Location: index.php?route=login');
                 exit;
