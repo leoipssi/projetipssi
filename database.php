a62e1a58db21d942022bd9ef3e5758fe382aac26
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-10 15:34:08 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,12 +1,10 @@
 <?php
 global $db;
-
 $host = 'localhost';
 $db_name = 'e_motion';
 $user = 'emotion_user';
 $pass = 'IPSSI2024';
 $charset = 'utf8mb4';
-
 $dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -19,7 +17,7 @@ try {
     error_log("Connexion à la base de données réussie");
 } catch (\PDOException $e) {
     error_log("Erreur de connexion PDO : " . $e->getMessage());
-    $db = null;
+    die("Erreur : La connexion à la base de données n'a pas pu être établie. Détails : " . $e->getMessage());
 }
 
 function is_db_connected() {
