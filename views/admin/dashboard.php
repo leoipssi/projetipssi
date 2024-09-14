a099ac74d7c4dea5b3fe3a543b075f9598043d99
Leo <97440909+DarQuiBball@users.noreply.github.com>
2024-09-14 22:23:59 +0200

    Update dashboard.php

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -3,8 +3,8 @@
 define('ROOT_PATH', realpath($_SERVER['DOCUMENT_ROOT'] . '/e-motion'));
 
 // Inclure les fichiers nécessaires
-require_once ROOT_PATH . '/models/User.php';
-require_once ROOT_PATH . '/models/Vehicule.php';
+require_once __DIR__ . '/../models/Users.php';
+require_once __DIR__ . '/../models/Vehicule.php';
 ?>
 
 <h1>Tableau de bord administrateur</h1>
